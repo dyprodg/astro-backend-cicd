c0613cd9ad693f0612602d9c6d67b216609d48d3
Dennis <dennisdiepolder@outlook.com>
2025-06-01 11:20:59 +0200

    push

---
+++ infra/main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_version = ">= 1.10"
+
+  backend "s3" {
+    bucket         = "astro-preset-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+# Hole die aktuelle AWS Account ID dynamisch
+data "aws_caller_identity" "current" {}
+
+# OIDC Provider (falls noch nicht vorhanden)
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_role" "astro_backend_cicd" {
+  name = "astro-backend-cicd-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = data.aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            # Erlaubt alle Branches deines Repos
+            "token.actions.githubusercontent.com:sub" = "repo:dyprodg/astro-backend-cicd:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Beispiel-Policy: S3 Vollzugriff auf den Frontend-Bucket (passe ggf. an)
+resource "aws_iam_role_policy" "astro_backend_cicd_policy" {
+  name = "astro-backend-cicd-policy"
+  role = aws_iam_role.astro_backend_cicd.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::astro-frontend-bucket",
+          "arn:aws:s3:::astro-frontend-bucket/*"
+        ]
+      }
+    ]
+  })
+}
